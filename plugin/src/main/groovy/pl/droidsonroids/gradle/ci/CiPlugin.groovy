23162caa363546d88e75a8c69401e6e115e22a39
Karol Wrótniak <koral--@users.noreply.github.com>
2015-06-22 02:26:12 +0200

    initial commit

---
+++ plugin/src/main/groovy/pl/droidsonroids/gradle/ci/CiPlugin.groovy
@@ -0,0 +1,41 @@
+package pl.droidsonroids.gradle.ci
+
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.AppPlugin
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.compile.JavaCompile
+
+public class CiPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.allprojects {
+            gradle.projectsEvaluated {
+                tasks.withType(JavaCompile) {
+                    options.compilerArgs << "-Xlint"
+                }
+            }
+        }
+        project.allprojects { subproject ->
+            subproject.plugins.withType(AppPlugin) {
+                def android = subproject.extensions.getByType(AppExtension)
+                android.signingConfigs {
+                    jenkinsRelease {
+                        storeFile new File("$System.env.HOME/.android/debug.keystore")
+                        storePassword 'android'
+                        keyAlias 'androiddebugkey'
+                        keyPassword 'android'
+                    }
+                }
+                android.buildTypes {
+                    jenkinsRelease {
+                        minifyEnabled true
+                        proguardFiles android.getDefaultProguardFile('proguard-android.txt'), 'proguard-rules.pro'
+                        signingConfig android.signingConfigs.jenkinsRelease
+                    }
+                }
+            }
+
+        }
+    }
+}
