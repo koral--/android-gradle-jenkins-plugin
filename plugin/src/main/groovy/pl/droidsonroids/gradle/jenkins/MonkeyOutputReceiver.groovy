78a0f78ceaac02e99b37c0e04a4fcc70849df0ed
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2015-08-26 22:10:26 +0200

    monkey output formatter added

--- plugin/src/main/groovy/pl/droidsonroids/gradle/jenkins/MonkeyOutputReceiver.groovy
+++ plugin/src/main/groovy/pl/droidsonroids/gradle/jenkins/MonkeyOutputReceiver.groovy
@@ -1,15 +1,22 @@
 package pl.droidsonroids.gradle.jenkins
 
 import com.android.ddmlib.MultiLineReceiver
-import groovy.transform.TupleConstructor
-import org.gradle.api.logging.Logger
 
 class MonkeyOutputReceiver extends MultiLineReceiver {
-    File file = new File("monkey.txt")
+
+    PrintWriter printWriter
+    MonkeyOutputReceiver() throws IOException {
+        printWriter = new PrintWriter(new File('monkey.txt'))
+    }
 
     @Override
     public void processNewLines(String[] lines) {
-        lines.each { file << it }
+        lines.each { printWriter.println(it) }
+    }
+
+    @Override
+    public void done() {
+        printWriter.close()
     }
 
     @Override
