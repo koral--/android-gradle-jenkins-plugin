96db711e2ac75f277fd6479fc20ee311fbaa3a05
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2016-09-27 04:19:35 +0200

    media files added, experimental support for API 23+

--- emulatorSetuper/src/main/groovy/pl/droidsonroids/gradle/jenkins/Setuper.groovy
+++ emulatorSetuper/src/main/groovy/pl/droidsonroids/gradle/jenkins/Setuper.groovy
@@ -4,27 +4,28 @@ import com.android.ddmlib.AndroidDebugBridge
 import com.android.ddmlib.IDevice
 import com.android.utils.StdLogger
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS
 import static java.util.concurrent.TimeUnit.SECONDS
 
 public class Setuper implements AndroidDebugBridge.IDeviceChangeListener {
 
-	public static final String ANIMATIONS_SQL_FILE_REMOTE_PATH = '/data/local/tmp/settings.sql'
+	private static final String ANIMATIONS_SQL_FILE_REMOTE_PATH = '/data/local/tmp/'
+	private static final String ANIMATOR_DURATION_SCALE = '\'<setting id="67" name="animator_duration_scale" value="0.0" package="android" />\''
+	private static final String WINDOW_ANIMATION_SCALE = '\'<setting id="18" name="window_animation_scale" value="0.0" package="android" />\''
+	private static final String TRANSITION_ANIMATION_SCALE = '\'<setting id="19" name="transition_animation_scale" value="0.0" package="android" />\''
+	private static final String MEDIA_SCAN_COMMAND = 'am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file://'
+
 	def outputReceiver = new LoggerBasedOutputReceiver(new StdLogger(StdLogger.Level.VERBOSE))
 	def bridge
-	String animationsSqlFilePath
+	private File tempDir
 
 	public static void main(String[] args) throws Exception {
 		new Setuper().setup()
 	}
 
 	Setuper() {
-		def animationsSqlFile = File.createTempFile('settings', 'sql')
-		animationsSqlFile.deleteOnExit()
-		animationsSqlFile << getClass().getResourceAsStream('settings.sql')
-		animationsSqlFilePath = animationsSqlFile.absolutePath
-
 		AndroidDebugBridge.initIfNeeded(false)
-
+		tempDir = File.createTempDir()
 		final androidHomeDir = System.getenv('ANDROID_HOME') ?: '/opt/android-sdk-update-manager'
 		def adbLocation = new File(androidHomeDir, 'platform-tools/adb')
 		bridge = AndroidDebugBridge.createBridge(adbLocation.absolutePath, false)
@@ -52,13 +53,46 @@ public class Setuper implements AndroidDebugBridge.IDeviceChangeListener {
 	}
 
 	def setup(IDevice device) {
-		if (!device.isOnline()) {
+
+		if (!device.online) {
 			return
 		}
-		device.pushFile(animationsSqlFilePath, ANIMATIONS_SQL_FILE_REMOTE_PATH)
+		String bootCompleted = null
+		int tryCount = 3
+		while (tryCount-- > 0 && bootCompleted != '1') {
+			bootCompleted = device.getSystemProperty('sys.boot_completed').get(Cleaner.ADB_COMMAND_TIMEOUT_MILLIS, SECONDS)
+		}
 
-		final remoteCommand = "sqlite3 /data/data/com.android.providers.settings/databases/settings.db < ${ANIMATIONS_SQL_FILE_REMOTE_PATH}"
-		device.executeShellCommand(remoteCommand, outputReceiver, Cleaner.ADB_COMMAND_TIMEOUT_MILLIS, SECONDS)
+		if (device.version.featureLevel >= 23) {
+			device.root()
+			executeRemoteCommand(device, "echo $ANIMATOR_DURATION_SCALE >> /data/system/users/0/settings_global.xml")
+			executeRemoteCommand(device, "echo $WINDOW_ANIMATION_SCALE >> /data/system/users/0/settings_system.xml")
+			executeRemoteCommand(device, "echo $TRANSITION_ANIMATION_SCALE >> /data/system/users/0/settings_system.xml")
+		} else {
+			pushFile(device, 'settings.sql', ANIMATIONS_SQL_FILE_REMOTE_PATH)
+			executeRemoteCommand(device, "sqlite3 /data/data/com.android.providers.settings/databases/settings.db < ${ANIMATIONS_SQL_FILE_REMOTE_PATH}")
+		}
+
+		for (name in ['image_portrait.jpg', 'image_square.jpg', 'video.mp4']) {
+			def file = pushFile(device, name, '/sdcard/')
+			executeRemoteCommand(device, "$MEDIA_SCAN_COMMAND$file")
+		}
+	}
+
+	private String pushFile(IDevice device, String fileName, String remotePath) {
+		def file = new File(tempDir, fileName)
+		if (!file.isFile()) {
+			file << getClass().getResourceAsStream(fileName)
+			file.deleteOnExit()
+		}
+
+		def remoteFilePath = remotePath + fileName
+		device.pushFile(file.path, remoteFilePath)
+		return remoteFilePath
+	}
+
+	private executeRemoteCommand(IDevice device, String remoteCommand) {
+		device.executeShellCommand(remoteCommand, outputReceiver, Cleaner.ADB_COMMAND_TIMEOUT_MILLIS, MILLISECONDS)
 	}
 
 	@Override
