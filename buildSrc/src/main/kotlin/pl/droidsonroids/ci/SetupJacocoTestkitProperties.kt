3b4e09404e8ed3f2bd40d91e511e5b722f0da692
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-03-18 02:38:41 +0100

    jacoco added to testkit

---
+++ buildSrc/src/main/kotlin/pl/droidsonroids/ci/SetupJacocoTestkitProperties.kt
@@ -0,0 +1,19 @@
+package pl.droidsonroids.ci
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+
+open class SetupJacocoTestkitProperties : DefaultTask() {
+    @OutputFile
+    val outputFile: File = project.file("${project.buildDir}/testkit/gradle.properties")
+
+    @TaskAction
+    fun createJacocoProperties() {
+        outputFile.mkdirs()
+        val jacocoRuntimePath = project.configurations.getByName("jacocoRuntime").asPath
+        val destFile = "${project.buildDir}/jacoco/testKit.exec"
+        outputFile.writeText("org.gradle.jvmargs:-javaagent:$jacocoRuntimePath=destfile=$destFile")
+    }
+}
--- plugin/src/test/kotlin/pl/droidsonroids/gradle/ci/LoggingOutputReceiverTest.kt
+++ plugin/src/test/kotlin/pl/droidsonroids/gradle/ci/LoggingOutputReceiverTest.kt
@@ -1,16 +1,39 @@
 package pl.droidsonroids.gradle.ci
 
 import com.android.utils.ILogger
-import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.never
 import com.nhaarman.mockito_kotlin.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
 import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnit
 
 class LoggingOutputReceiverTest {
+    @get:Rule
+    val mockitoRule = MockitoJUnit.rule()
+    @Mock
+    lateinit var logger: ILogger
+    @InjectMocks
+    lateinit var receiver: LoggingOutputReceiver
+
     @Test
-    fun processNewLines() {
-        val logger = mock<ILogger>()
-        LoggingOutputReceiver(logger).processNewLines(arrayOf("a", "b"))
+    fun `new lines passed to logger`() {
+        receiver.processNewLines(arrayOf("a", "b"))
         verify(logger).info("a")
         verify(logger).info("b")
+    }
+
+    @Test
+    fun `no lines not passed to logger`() {
+        receiver.processNewLines(emptyArray())
+        verify(logger, never()).info(any())
+    }
+
+    @Test
+    fun `is not cancelled by default`() {
+        assertThat(receiver.isCancelled).isFalse()
     }
 }
